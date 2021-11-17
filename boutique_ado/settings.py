49f510bf143104d6619bf63af2fe4bc2f7f48287
maialenz <maialenzumeta@gmail.com>
2021-11-17 18:46:32 +0000

    testing heroku deployment

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = "DEVELOPMENT" in os.environ
 
-ALLOWED_HOSTS = ['mai-django-boutique-ado.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['mai-django-boutique-ado.herokuapp.com', '']
 
 
 # Application definition
