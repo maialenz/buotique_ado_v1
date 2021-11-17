a7e91072c31d4fc8a0e4520a5f1bf64a4d5065af
maialenz <maialenzumeta@gmail.com>
2021-11-17 17:56:11 +0000

    restart heroku

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -123,16 +123,15 @@ WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+        'default': dj_database_url.parse()
     }
 else:
     DATABASES = {
          'default': {
              'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
+             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+            }
     }
-
 
 
 # Password validation
